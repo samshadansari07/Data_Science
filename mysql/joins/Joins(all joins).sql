8f9778cd7a8a28e23afe13d5becdb55f3694d3db
samshadansari07 <samshadcse2022@gmail.com>
2026-06-21 13:38:03 +0530

    add some inner join questions

--- mysql/joins/Joins(all joins).sql
+++ mysql/joins/Joins(all joins).sql
@@ -44,9 +44,62 @@ select firstname, lastname , productname from employees cross join products;
 
 -- ___________________inner joins________
 
--- 
+-- 1. Display customer name and their sales representative’s
+-- first name.
+select customername, firstname from customers c inner join employees e
+on c.salesrepemployeenumber = e.employeenumber;
+
+-- 2. List order number and order date along with the
+-- customer name.
+
+select customername, ordernumber , orderdate from customers inner join orders 
+using(customernumber);
+
+-- 3. Show employee first name, last name, and office city.
+select concat(firstname," ",lastname) as empfillname , city from employees inner join offices
+using(officecode);
+
+-- 4. Display product name and product line.. 
+select productname , productline from products;
+
+-- 5. Show order number and product code from order
+-- details.
+select ordernumber , productcode from orderdetails inner join products
+using(productcode);
+
+-- 6. List customer name and check number for each
+-- payment.
+
+select customername , checknumber from customers inner join payments 
+using(customernumber);
+
+-- 7. Display product name and buy price.  
+select productname , buyprice from products ;
+
+-- 8. Show employee name and job title along with their
+-- office country.
+select concat(firstname ," ",lastname)as empfullname,
+jobtitle , country from employees inner join offices 
+using(officecode);
+
+-- 9. Display order number, product name, and quantity
+-- ordered.
+
+select ordernumber , productname, quantityordered from orders inner join
+orderdetails using(ordernumber) inner join products using(productcode);
+
+-- 10. Show customer name, order number, and order
+-- status for only Shipped orders.
+
+select customername, ordernumber, status from orders inner join customers 
+using(customernumber) where status = "shipped";
 
 
+
+
+
+
+ 
 
 
 
